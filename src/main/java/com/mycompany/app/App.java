ec14815abb983cbdd4a6b1144944d35015855193
Nitesh Hibare <niteshhibare02@gmail.com>
2020-10-15 17:21:16 +0530

    just modified app.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,12 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World1!";
 
     public App() {}
 
     public static void main(String[] args) {
+		System.out.println("Hello"));
         System.out.println(new App().getMessage());
     }
 
